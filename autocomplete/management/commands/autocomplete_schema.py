db7fce625a346f6692f5f479fcbd1a418945e40f
Charles Leifer <coleifer@gmail.com>
2010-12-26 13:13:57 -0600

    Fixing bugs in the mgmt command

--- autocomplete/management/commands/autocomplete_schema.py
+++ autocomplete/management/commands/autocomplete_schema.py
@@ -2,7 +2,7 @@ import os
 from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
-from django.template import loader, render_to_string
+from django.template import loader
 
 
 class Command(BaseCommand):
@@ -12,17 +12,18 @@ class Command(BaseCommand):
     )
     help = 'Generate a schema useful for autocomplete'
     
-    def write_template(self, template_name):
-        t = loader.select_template(template_name)
-        fh = open(template_name.replace('.conf', ''), 'w')
-        fh.write(render_to_string(t, {}))
+    def write_template(self, template_dir, template_name):
+        filename = template_name.replace('.conf', '')
+        fh = open(filename, 'w')
+        fh.write(loader.render_to_string(template_dir + '/' + template_name, {}))
         fh.close()
+        print 'Successfully wrote [%s]' % filename
 
     def handle(self, **options):
-        directory = options.get('directory', '.')
+        directory = options.get('directory') or '.'
         try:
             os.chdir(directory)
         except OSError:
             raise CommandError('Error changing directory to %s' % directory)
 
-        self.write_template('autocomplete/schema.xml.conf')
+        self.write_template('autocomplete', 'schema.xml.conf')
